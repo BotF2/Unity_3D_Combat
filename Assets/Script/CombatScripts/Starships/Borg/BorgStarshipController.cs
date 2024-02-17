04df81402b2574ae3837b45b783f5431388de0ba
Kenneth Flanagan <kenneth.of.borg@gmail.com>
2024-02-17 16:35:29 -0700

    Revert "Importing assets and code with updated namespaces"
    This reverts commit e04eb0fa60f7c42eff87ecca44aae085999f42e9.

--- Assets/Script/CombatScripts/Starships/Borg/BorgStarshipController.cs
+++
@@ -1,6 +0,0 @@
-namespace Assets.SpaceCombat.AutoBattle.Scripts.Starships.Borg
-{
-    public class BorgStarshipController : StarshipController
-    {
-    }
-}
--- Assets/Script/CombatScripts/Starships/States/IdleState.cs
+++
@@ -1,8 +0,0 @@
-﻿using Assets.Plugins.YAFSM;
-
-namespace Assets.SpaceCombat.AutoBattle.Scripts.Starships.States
-{
-    public class IdleState : State
-    {
-    }
-}
