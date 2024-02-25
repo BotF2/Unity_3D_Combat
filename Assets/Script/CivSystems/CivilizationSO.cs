e909afc8107e4cf71822fae841c36c51398ce25b
Kenneth Flanagan <kenneth.of.borg@gmail.com>
2024-02-25 16:24:56 -0700

    minor change for transfer to labtop

---
+++ Assets/Script/CivSystems/CivilizationSO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using Assets.Core;
+
+public class CivilizationSO : ScriptableObject
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
