47a77c25851731d92776d7dc0d3c41bdb61a24bb
kflanagan <kenneth.of.borg@gmail.com>
2024-02-27 10:46:50 -0700

    Earlly Civilization class/file setup

--- Assets/Script/CivSystems/CivilizationSO.cs
+++ Assets/Script/CivSystems/CivilizationSO.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 using Assets.Core;
+using UnityEngine.Analytics;
 
 public class CivilizationSO : ScriptableObject
 {
-
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "CivSO", menuName = "Civ")]
+    public class CivSO : ScriptableObject
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public int CivInt;
+        public int CivEnum;
+        public string CivShortName;
+        public string CivLongName;
+        public int CivHomeSystem;
+        public string TraitOne;
+        public string TraitTwo;
+        public string CivImage;
+        public string Insignia;
+        public int Population;
+        public int Credits;
+        public int TechPoints;
+        public string Description;
     }
 }
+
